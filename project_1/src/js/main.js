4f7cb8769641ad6ec92e97bcec12a1e9ab2603c6
fellainthewagon <fellainthewagon@gmail.com>
2021-04-12 14:50:32 +0300

    project1 finish

--- project_1/src/js/main.js
+++ project_1/src/js/main.js
@@ -3,11 +3,15 @@ import modals from "./modules/modals";
 import tabs from "./modules/tabs";
 import forms from "./modules/forms";
 import changeModalState from "./modules/changeModalState";
+import timer from "./modules/timer";
+import images from "./modules/images";
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
 
   let modalState = {};
+  let deadline = "2021-04-15";
+
   changeModalState(modalState);
   modals();
   tabs(".glazing_slider", ".glazing_block", ".glazing_content", "active");
@@ -25,4 +29,6 @@ window.addEventListener("DOMContentLoaded", () => {
     "inline-block"
   );
   forms(modalState);
+  timer(".container1", deadline);
+  images();
 });
