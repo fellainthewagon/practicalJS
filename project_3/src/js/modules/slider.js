76f5480005f18ee2193586b027c902fddf525402
fellainthewagon <fellainthewagon@gmail.com>
2021-04-15 23:51:22 +0300

    project3_part1

---
+++ project_3/src/js/modules/slider.js
@@ -0,0 +1,66 @@
+export default class Slider {
+  constructor(page, btns) {
+    this.page = document.querySelector(page);
+    this.slides = this.page.children;
+    this.btns = document.querySelectorAll(btns);
+    this.slideIndex = 1;
+  }
+
+  showSlides(n) {
+    if (n > this.slides.length) {
+      this.slideIndex = 1;
+    }
+    if (n < 1) {
+      this.slideIndex = this.slides.length;
+    }
+
+    //hanson on page 3
+    try {
+      this.hanson.style.opacity = "0";
+      if (n === 3) {
+        this.hanson.classList.add("animated");
+
+        setTimeout(() => {
+          this.hanson.style.opacity = "1";
+          this.hanson.classList.add("slideInUp");
+        }, 3000);
+      } else {
+        this.hanson.classList.remove("slideInUp");
+      }
+    } catch (e) {}
+
+    // ------- main func
+    this.slides.forEach((slide) => {
+      slide.style.display = "none";
+    });
+
+    this.slides[this.slideIndex - 1].style.display = "block";
+  }
+
+  plusSlides(n) {
+    this.showSlides((this.slideIndex += n));
+  }
+
+  render() {
+    //hanson on page 3
+    try {
+      this.hanson = document.querySelector(".hanson");
+    } catch (e) {}
+
+    // --------- main func
+    this.btns.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        this.plusSlides(1);
+      });
+
+      //click on logo
+      btn.parentNode.previousElementSibling.addEventListener("click", (e) => {
+        e.preventDefault;
+        this.slideIndex = 1;
+        this.showSlides(this.slideIndex);
+      });
+    });
+
+    this.showSlides(this.slideIndex);
+  }
+}
